aa24cf9e8beca73e0c0c08a2914ac1cb00d8892f
Alina Goncharova <goncarovaalina206@gmail.com>
2021-05-09 15:55:09 +0300

    get by id

--- contacts.js
+++ contacts.js
@@ -16,3 +16,30 @@ const listContacts = async () => {
     console.log(err);
   }
 };
+
+const getContactById = async (contactId) => {
+  try {
+    const data = await fs.readFile(contactsPath);
+    const parsedContacts = JSON.parse(data);
+    const contactById = parsedContacts.find(
+      (contact) => contact.id === contactId
+    );
+
+    if (parsedContacts.length === 0) {
+      return console.log(`No contacts`);
+    }
+    if (!contactById) {
+      return console.log(`Contact with id: ${contactId} is not found`);
+    }
+    console.table(contactById);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+module.exports = {
+  listContacts,
+  getContactById,
+  // addContact,
+  // removeContact,
+};
