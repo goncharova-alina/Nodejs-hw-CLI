322d497f8d0250d2e1cd7e666a4a289901d793d9
Alina Goncharova <goncarovaalina206@gmail.com>
2021-05-10 11:53:05 +0300

    add images

--- contacts.js
+++ contacts.js
@@ -36,10 +36,54 @@ const getContactById = async (contactId) => {
     console.log(err);
   }
 };
+const addContact = async (name, email, phone) => {
+  try {
+    const data = await fs.readFile(contactsPath);
+    const parsedContacts = JSON.parse(data);
+    const id = parsedContacts[parsedContacts.length - 1].id + 1;
+
+    const newContact = { id, name, email, phone };
+
+    parsedContacts.push(newContact);
+
+    await fs.writeFile(contactsPath, JSON.stringify(parsedContacts));
+
+    console.log(`Contact was added`);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const removeContact = async (contactId) => {
+  try {
+    const data = await fs.readFile(contactsPath);
+    const parsedContacts = JSON.parse(data.toString());
+
+    if (parsedContacts.length === 0) {
+      return console.log(`No contacts`);
+    }
+
+    const contactById = parsedContacts.find(
+      (contact) => contact.id === contactId
+    );
+    if (!contactById) {
+      return console.log(`Contact with id: ${contactId} is not found`);
+    }
+
+    const contacts = parsedContacts.filter(
+      (contact) => contact.id !== contactId
+    );
+
+    await fs.writeFile(contactsPath, JSON.stringify(contacts));
+    console.log(`Contact with id: ${contactId} was deleted`);
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 module.exports = {
   listContacts,
   getContactById,
-  // addContact,
-  // removeContact,
+  addContact,
+  removeContact,
 };
